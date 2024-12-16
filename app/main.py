95a0195815431a51a43783ed7baca3250054bf64
xosnrdev <successxodev@gmail.com>
2024-12-16 18:29:07 -0500

    chore: added our first test with the health check endpoint

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import platform
+import datetime
+
+app = FastAPI(
+    title="E-Library API System",
+    description="API for managing an online library system",
+)
+
+
+@app.get("/")
+def health_check():
+    return {
+        "name": app.title,
+        "version": app.version,
+        "description": app.description,
+        "system": {
+            "os": platform.system(),
+            "os_version": platform.version(),
+            "python_version": platform.python_version(),
+            "machine": platform.machine(),
+            "processor": platform.processor(),
+        },
+        "timestamp": datetime.datetime.now().isoformat(),
+        "status": "operational",
+    }
